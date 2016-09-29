cb325b26c3d0670eb48990a5828ee16281767c26
PRIYANKAPS92 <priyankasreekala@gmail.com>
2016-09-29 14:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/article-one', function (req, res) {
   res.send('article-one requested will be served here...');
+  
+});
+app.get('/article-two', function (req, res) {
+  res.send('article-twp requested will be served here...');
+  
+});
+app.get('/article-three', function (req, res) {
+  res.send('article-three requested will be served here...');
   
 });
 
