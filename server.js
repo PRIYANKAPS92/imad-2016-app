3d87fc57bc1ffdde92b8abcf3017b9efd8bbf9c1
PRIYANKAPS92 <priyankasreekala@gmail.com>
2016-09-29 14:50:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile('article-one requested will be seved here...');
+  res.sendFile('article-one requested will be served here...');
   
 });
 
